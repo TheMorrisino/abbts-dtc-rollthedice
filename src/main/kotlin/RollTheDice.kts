af260f66d1d8775f1b141963d3e9e776bd1e1ba8
Andreas Wassmer <andreas.wassmer@fhnw.ch>
2025-11-18 20:36:14 +0100

    first commit

---
+++ src/main/kotlin/RollTheDice.kts
@@ -0,0 +1,31 @@
+import kotlin.random.Random
+
+/**
+ * Simuliert ein einfaches Würfelspiel zwischen dem
+ * Computer und einem Spieler
+ *
+ * Autor: Andreas Wassmer
+ * Version: 20241129
+ */
+
+fun main() {
+
+    val aDie = Random
+
+    val playerScore = aDie.nextInt(7)
+    val computerScore = aDie.nextInt(7)
+
+    // Todo: Den Fehler im Programm finden und beheben
+    // Todo: Den Spieler nach dem Namen fragen
+    // Todo: Solange spielen bis Spieler abbricht
+    // Todo: Ausgeben, wer mehr Runden gewonnen hat
+
+    println("Du würfelst: $playerScore  --  Computer würfelt: $computerScore")
+    when {
+        playerScore > computerScore -> println("Du gewinnst")
+        playerScore < computerScore -> println("Der Computer gewinnt")
+        else -> println("Unentschieden")
+    }
+}
+
+main()
